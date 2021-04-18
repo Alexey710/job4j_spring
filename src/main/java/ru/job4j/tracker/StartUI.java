20efdf12240b7ca2d2bed5fc24fbb36536a6ab3d
Aleksey Krylov <elsite2015@yandex.ru>
2021-04-18 11:41:07 +0400

    0. Что такое DI? [#477066]

---
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -0,0 +1,23 @@
+package ru.job4j.tracker;
+
+public class StartUI {
+
+    private Store store;
+    private ConsoleInput input;
+
+    public StartUI(Store store, ConsoleInput input) {
+        this.store = store;
+        this.input = input;
+    }
+
+    public void add() {
+        String value = input.askStr("Введите значение:");
+        store.add(value);
+    }
+
+    public void print() {
+        for (String value : store.getAll()) {
+            System.out.println(value);
+        }
+    }
+}
